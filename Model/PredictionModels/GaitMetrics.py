d5ae8df431d1927186265029e328ca2a85793cb4
Thivirug <95113663+Thivirug@users.noreply.github.com>
2025-04-17 20:44:32 +1000

    minor change

--- Model/PredictionModels/GaitMetrics.py
+++ Model/PredictionModels/GaitMetrics.py
@@ -2,7 +2,7 @@
 import os, sys; [(sys.path.append(d), print(f'Added {d} to system path')) for d in (os.path.abspath(os.path.join(os.getcwd(), *([".."] * i))) for i in range(len(os.getcwd().split(os.sep)))) if os.path.isfile(os.path.join(d, 'main.py'))]
 
 from Model.Pose.Sequence.PoseFrame import Landmark, PoseFrame
-from typing import Dict, List
+from typing import Dict, List, Tuple
 class GaitMetrics:
     """
         Calculate gait metrics for a particular gait type.
@@ -11,7 +11,7 @@ class GaitMetrics:
         self.landmarks = landmarks 
         self.type = gait_type
 
-    def parse_landmarks(self, required_landmarks: List[int]) -> List[Landmark]:
+    def get_landmarks(self, required_landmarks: List[int]) -> List[Landmark]:
         metric_landmarks = []
 
         for landmarks_list in self.landmarks.values():
@@ -25,14 +25,14 @@ class GaitMetrics:
 
         return metric_landmarks
 
-    # TODO: Change the metric names to what we have
+    # TODO: Change the func names to gait metric name and add the equations for each metric
     
     def metric1(self, required_landmarks: List[int]):
         """
             Calculate the ______ metric using the required landmarks.
         """
         # Parse the landmarks to get the required ones
-        metric_landmarks = self.parse_landmarks(required_landmarks)
+        metric_landmarks = self.get_landmarks(required_landmarks)
 
         # Perform calculations on the metric_landmarks to get the desired metric
         metric_value = 0.0
